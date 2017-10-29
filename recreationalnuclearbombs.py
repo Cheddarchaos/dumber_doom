373ba9d470cf27675375af5330d29922b9bf5781
Cheddarchaos <coreyansley@gmail.com>
2017-10-29 09:42:34 +0000

    Add files via upload

---
+++ recreationalnuclearbombs.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import math
+import random
+import aim_at_point
+
+
+def Aimbot(target, list_pl, shots):
+
+    from pprint import pprint as pp
+
+    connect = "http://localhost:6001/api/player"
+
+    r = requests.get(connect)
+
+    Xtarget = []
+    Ytarget = []
+    Targetlist = []
+
+    for entity in list_pl:
+        if entity["type"] == target and entity["console-player"]:
+            Targetlist.append(entity["id"])
+            tempo = (entity["position"])
+            Xtarget.append(tempo["x"])
+            Ytarget.append(tempo["y"])
+    if len(Xtarget) > 0:
+        minX = Xtarget[0]
+        minY = Ytarget[0]
+        minangle = aim_at_point.find_angle(minX, minY)
+        for i in range(len(Xtarget)):
+            angle = aim_at_point.find_angle(Xtarget[i],Ytarget[i])
+            if angle < minangle:
+                minX = Xtarget[i]
+                minY = Ytarget[i]
+
+        aim_at_point.aim_point(minX,minY)
+
+
+        for jeff in range(shots):
+            for elt in Targetlist:
+                print("x")
+                derpstring = 'http://localhost:6001/api/world/los/0/%s' % elt
+                lineos = requests.get(derpstring)
+                memes = lineos.text
+                lineos2 = json.loads(memes)
+                print("y")
+                if "los" in lineos2:
+                    if lineos2["los"]:
+                        requests.post('http://localhost:6001/api/player/actions', json={"type": "shoot","amount": 3})
+                print("z")
+
+""""
+for i in range(2):
+
+    Aimbot("Barrel",list_obj)
+"""
